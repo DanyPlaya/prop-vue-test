0eb4771a5997a7a87e852883cbbbcac8143f8c23
Dania <danila.lusuy@gmail.com>
2024-09-26 23:19:34 +0300

    feat: add FileUploader,DataTable,ExportToCsv,ExportToXlsx and charts

--- src/pages/main/ui/main.tsx
+++ src/pages/main/ui/main.tsx
@@ -1,3 +1,33 @@
+"use client";
+import { ProductType } from "@/entities/product";
+import { ExportToCsv } from "@/features/export-to-csv";
+import { ExportToXlsx } from "@/features/export-to-xlsx";
+import { FileUploader } from "@/features/file-uploader";
+import { AveragePriceChart } from "@/widgets/average-price-chart";
+import { columns, DataTable } from "@/widgets/data-table";
+import { useState } from "react";
+
 export const MainPage = () => {
-  return <div>mainPage</div>;
+  const [productData, setProductData] = useState<ProductType[]>([]);
+  console.log(productData);
+  return (
+    <div className="grid gap-4">
+      <FileUploader onFileUpload={setProductData} />
+      <div className="grid ">
+        <DataTable
+          setData={setProductData}
+          columns={columns}
+          data={productData || []}
+        />
+
+        {productData.length > 0 && (
+          <div className="flex items-center justify-center gap-10">
+            <ExportToCsv productData={productData} />
+            <ExportToXlsx productData={productData} />
+          </div>
+        )}
+        {productData.length > 0 && <AveragePriceChart data={productData} />}
+      </div>
+    </div>
+  );
 };
